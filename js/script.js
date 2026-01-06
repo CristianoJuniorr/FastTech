38de715c0f888293092ad2214226ffb7fe32927f
lucasvaz-debug <lucasvaz.nv@gmail.com>
2026-01-05 23:58:28 -0300

    Inclusão e ajuste de Banners

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* ================= WHATSAPP ================= */
 function comprar(produto) {
   const numero = "5563992298058";
   const mensagem = `Olá! Vi o ${produto} no site da Fast Tech e gostaria de mais informações.`;
@@ -5,3 +6,56 @@ function comprar(produto) {
 
   window.open(url, "_blank");
 }
+
+/* ================= SLIDER DE BANNER ================= */
+const banners = document.querySelectorAll(".banner");
+const prevBtn = document.querySelector(".banner-prev");
+const nextBtn = document.querySelector(".banner-next");
+
+let index = 0;
+let intervalo;
+
+/* Mostrar banner ativo */
+function showBanner(i) {
+  banners.forEach((banner) => banner.classList.remove("active"));
+  banners[i].classList.add("active");
+}
+
+/* Próximo banner */
+function nextBanner() {
+  index = (index + 1) % banners.length;
+  showBanner(index);
+}
+
+/* Banner anterior */
+function prevBanner() {
+  index = (index - 1 + banners.length) % banners.length;
+  showBanner(index);
+}
+
+/* Auto play */
+function startAutoPlay() {
+  intervalo = setInterval(nextBanner, 4000);
+}
+
+/* Reset autoplay ao clicar */
+function resetAutoPlay() {
+  clearInterval(intervalo);
+  startAutoPlay();
+}
+
+/* Eventos dos botões */
+if (nextBtn && prevBtn) {
+  nextBtn.addEventListener("click", () => {
+    nextBanner();
+    resetAutoPlay();
+  });
+
+  prevBtn.addEventListener("click", () => {
+    prevBanner();
+    resetAutoPlay();
+  });
+}
+
+/* Inicia */
+startAutoPlay();
